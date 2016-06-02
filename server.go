78279886257e90db4c5778130c418e53831adc2f
Fabio Rehm <fgrehm@gmail.com>
2016-06-02 00:17:55 -0300

    Accept a file payload instead of text

--- server.go
+++ server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"strconv"
@@ -24,23 +25,19 @@ func runServer(port string) {
 		return renderTemplate(c, r, http.StatusOK, "form", nil)
 	})
 	e.POST("/", func(c echo.Context) error {
-		sourceFileHeader, err := c.FormFile("file")
+		input, err := openUploadedFile(c, "input")
 		if err != nil {
 			return err
 		}
-		sourceFile, err := sourceFileHeader.Open()
-		if err != nil {
-			return err
-		}
-		defer sourceFile.Close()
+		defer input.Close()
 
-		lsbsToUse, err := strconv.Atoi(c.FormValue("lsbs"))
+		payload, err := openUploadedFile(c, "payload")
 		if err != nil {
 			return err
 		}
+		defer payload.Close()
 
-		payload := c.FormValue("payload")
-		id, err := embedStrPayloadOnUploadedFile(payload, sourceFile, lsbsToUse)
+		id, err := processUpload(input, payload)
 		if err != nil {
 			return err
 		}
@@ -51,36 +48,70 @@ func runServer(port string) {
 		id := c.Param("id")
 		return renderTemplate(c, r, http.StatusOK, "result", id)
 	})
+	e.GET("/:id/:lsbs", func(c echo.Context) error {
+		id := c.Param("id")
+		lsbs := c.Param("lsbs")
+		workingDir := "wavs/" + id
+		filePath := fmt.Sprintf("%s/output-%s.wav", workingDir, lsbs)
+
+		if _, err := os.Stat(filePath); os.IsNotExist(err) {
+			payload, err := ioutil.ReadFile(workingDir + "/payload.bin")
+			if err != nil {
+				return err
+			}
+			lsbsToUse, err := strconv.Atoi(lsbs)
+			if err != nil {
+				return err
+			}
+			err = encode(workingDir + "/input.wav", filePath, lsbsToUse, payload)
+			if err != nil {
+				return err
+			}
+		}
+
+		return c.File(filePath)
+	})
+	// TODO: REMOVE
 	e.Static("/wavs", "wavs")
 	e.Run(standard.New(":" + port))
 }
 
-func embedStrPayloadOnUploadedFile(payload string, sourceFile io.Reader, lsbsToUse int) (string, error) {
+func openUploadedFile(c echo.Context, fileField string) (io.ReadCloser, error) {
+	fileHeader, err := c.FormFile(fileField)
+	if err != nil {
+		return nil, err
+	}
+	file, err := fileHeader.Open()
+	if err != nil {
+		return nil, err
+	}
+	return file, nil
+}
+
+func processUpload(input, payload io.Reader) (string, error) {
 	id := uuid.NewV4().String()
 	outputDir := "wavs/" + id
 	os.MkdirAll(outputDir, 0775)
 
-	inputPath := outputDir + "/input.wav"
-	outputPath := fmt.Sprintf("%s/output-%d.wav", outputDir, lsbsToUse)
-
-	// Write source file to output dir
-	inputFile, err := os.Create(inputPath)
-	if err != nil {
+	if err := writeUploadedFile(input, outputDir+"/input.wav"); err != nil {
 		return "", err
 	}
-	defer inputFile.Close()
-
-	// Copy
-	if _, err = io.Copy(inputFile, sourceFile); err != nil {
-		return "", err
-	}
-
-	// Encode it
-	if err := encode(inputPath, outputPath, lsbsToUse, []byte(payload)); err != nil {
+	if err := writeUploadedFile(payload, outputDir+"/payload.bin"); err != nil {
 		return "", err
 	}
 
 	return id, nil
+}
+
+func writeUploadedFile(source io.Reader, path string) error {
+	file, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	_, err = io.Copy(file, source)
+
+	return err
 }
 
 func renderTemplate(c echo.Context, r *render.Render, status int, name string, binding interface{}) error {
